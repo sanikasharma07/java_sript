06ad4e8d2c8aec2c2182136195c40a9bf4d14f07
sanikasharma07 <vanshikabuilddevelopers@gmail.com>
2026-05-25 05:58:23 +0000

    video1 done

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+ const accountId= 144553
+ let accountEmail="sanika@google.com"
+ var accountPassword="12345"
+ accountCity="Jaipur"
+ let accountState;
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+ /*
+  prefer let dont use var
+ */
