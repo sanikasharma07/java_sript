8357424ee3a91780fe5ca66bdc06e30f7061708e
sanikasharma07 <vanshikabuilddevelopers@gmail.com>
2026-05-25 09:49:18 +0000

    operations

---
+++ basics/operations.js
@@ -0,0 +1,19 @@
+let value=3;
+let negvalue=-value;
+console.log(2/3);
+// 2**3
+//2%3
+//2*2 
+let str1="Hello"
+let str2=" Sanika"
+let str3=str1+str2
+console.log(str3)
+// "1"+2-> gives 12
+// 1+"2"-> gives 12
+// "1"+2+2-> gives 122
+// 1+2+"2"-> gives 32
+console.log(1+2+"2"+"4"+"1")// not preferable
+ // console.log(true)->true
+ //console.log(+true)->1
+ // console.log(true+)->error
+ 
