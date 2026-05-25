ff77264b71c288e60aefd4f08591237bbe58ff08
sanikasharma07 <vanshikabuilddevelopers@gmail.com>
2026-05-25 08:32:41 +0000

    video2 done

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // this treat all js code as newer version
+// alert(3+3)// we are using node js,not browser so this is invalid  
+
+// number-> range 2 to the power 53
+// bigint -> for very big numbers(very rare)
+// string->'' or ""("preferable")
+// boolean-> true/false
+// null-> standalone value(this is a certain value that is you can assign something=null)
+// undefined-> when no value is assigned to a variable
+// symbol-> for uniqueness
+// object
+let age=18
+console.log(typeof "Hitesh");
+console.log(typeof age);
+console.log(typeof null);// object
+console.log(typeof undefined);//undefined
