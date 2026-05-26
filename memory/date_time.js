3c9d2af03269c46124160d59cf65fc3f83c6fcc6
sanikasharma07 <vanshikabuilddevelopers@gmail.com>
2026-05-26 19:20:03 +0000

    8:11

---
+++ memory/date_time.js
@@ -0,0 +1,7 @@
+//temporal->still not a global object but in future can be
+let newDate= new Date()
+console.log(newDate.toString())
+console.log(newDate.toDateString())
+console.log(newDate.toLocaleString())
+console.log(newDate.toJSON())
+console.log(typeof newDate)
