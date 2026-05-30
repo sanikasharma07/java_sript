a49cf87e5fce7e81430868bfd3aea87061bf7e9e
sanikasharma07 <vanshikabuilddevelopers@gmail.com>
2026-05-30 13:14:28 +0000

    7:57

---
+++ loops/one.js
@@ -0,0 +1,22 @@
+// for loop
+// while do while
+let score=1
+do{
+    console.log(`${score}`);
+   score+=2;
+}while(score<=10);
+//*******high order array loops**********
+// for of loop
+//for(const of object)
+const arr=[1,2,3,4]
+for( const num of arr){
+    console.log(`${num}`)
+}
+// here no increment needed 
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+// maps
+ 
