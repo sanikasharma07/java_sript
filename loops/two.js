4c3b68b513fde97044f257b58a244333e38b8fb9
sanikasharma07 <vanshikabuilddevelopers@gmail.com>
2026-05-31 10:16:30 +0000

    done

--- loops/two.js
+++ loops/two.js
@@ -1,6 +1,38 @@
  // for each loop
  const coding = ["js", "ruby", "java", "python", "cpp"]
 
-// coding.forEach( function (val){
-//     console.log(val);
+//  coding.forEach( function (val){
+//      console.log(val);
+//  } )
+// coding.forEach( (item) => {
+//     console.log(item);
 // } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)  // dont do print me()
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )// this will print values,index and arr
+ const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
